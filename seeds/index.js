5050ce095252e023706b0af542bf5c1b62269938
Abhishek Anand <abhishekanand1138@gmail.com>
2021-12-29 05:29:03 +0530

    "Added UI improvements"

--- seeds/index.js
+++ seeds/index.js
@@ -17,9 +17,14 @@ seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 30) + 10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
+            image: "https://source.unsplash.com/collection/483251",
+            description:
+                "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nostrum nulla distinctio ut quod rerum, adipisci ex quos officia exercitationem vitae dolore accusamus at recusandae nam, facere inventore fuga rem necessitatibus! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nostrum nulla distinctio ut quod rerum, adipisci ex quos officia exercitationem vitae dolore accusamus at recusandae nam, facere inventore fuga rem necessitatibus!",
+            price,
         });
         await camp.save();
     }
