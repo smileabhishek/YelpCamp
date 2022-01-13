35cc7942293352bc3498fdf9b47a4dc97775f0dc
Abhishek Anand <abhishekanand1138@gmail.com>
2022-01-14 04:18:39 +0530

    "added session storage and flash messages"

--- app.js
+++ app.js
@@ -2,19 +2,23 @@ const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
 const ejsMate = require("ejs-mate");
+const session = require("express-session");
+const flash = require("connect-flash");
 const ExpressError = require("./utils/ExpressError");
 const methodOverride = require("method-override");
 
+// importing routers
 const campgrounds = require("./routes/campgrounds");
 const reviews = require("./routes/reviews");
+
 // conecting to mogngodb
 mongoose.connect("mongodb://localhost:27017/yelp-camp");
-
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
   console.log("Database connected");
 });
+
 // starting app
 const app = express();
 
@@ -24,26 +28,51 @@ app.set("views", path.join(__dirname, "views"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+// serving static files directory
 app.use(express.static(path.join(__dirname, "public")));
 
+// sessions configurations
+const sessionConfig = {
+  secret: "thisisadumbwaytokeepsecret",
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    httpOnly: true,
+    expires: Date().now + 1000 * 60 * 60 * 24 * 7,
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+  },
+};
+app.use(session(sessionConfig));
+
+// using flash service
+app.use(flash());
+app.use((req, res, next) => {
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
+  next();
+});
+// using routers
 app.use("/campgrounds", campgrounds);
 app.use("/campgrounds/:id/reviews", reviews);
 
+// home route
 app.get("/", (req, res) => {
   res.render("home");
 });
 
-// error routes
+// not existing routes routes
 app.all("*", (req, res, next) => {
   next(new ExpressError("Page Not Found!", 404));
 });
 
+// error handling middleware
 app.use((err, req, res, next) => {
   const { statusCode = 500 } = err;
   if (!err.message) err.message = "Oh No! Something went wrong...";
   res.status(statusCode).render("error", { err });
 });
 
+// server listener
 app.listen(3000, () => {
   console.log("serving on port 3000");
 });
