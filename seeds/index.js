32966482dc0e9838e21c3e1abd367d188139d740
Abhishek Anand <abhishekanand1138@gmail.com>
2022-01-20 04:36:58 +0530

    "added author of each post"

--- seeds/index.js
+++ seeds/index.js
@@ -9,27 +9,28 @@ mongoose.connect("mongodb://localhost:27017/yelp-camp");
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
-    console.log("Database connected");
+  console.log("Database connected");
 });
 
 const sample = (array) => array[Math.floor(Math.random() * array.length)];
 seedDB = async () => {
-    await Campground.deleteMany({});
-    for (let i = 0; i < 50; i++) {
-        const random1000 = Math.floor(Math.random() * 1000);
-        const price = Math.floor(Math.random() * 30) + 10;
-        const camp = new Campground({
-            location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`,
-            image: "https://source.unsplash.com/collection/483251",
-            description:
-                "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nostrum nulla distinctio ut quod rerum, adipisci ex quos officia exercitationem vitae dolore accusamus at recusandae nam, facere inventore fuga rem necessitatibus! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nostrum nulla distinctio ut quod rerum, adipisci ex quos officia exercitationem vitae dolore accusamus at recusandae nam, facere inventore fuga rem necessitatibus!",
-            price,
-        });
-        await camp.save();
-    }
+  await Campground.deleteMany({});
+  for (let i = 0; i < 50; i++) {
+    const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 30) + 10;
+    const camp = new Campground({
+      author: "61e737a4091907bf96d15595",
+      location: `${cities[random1000].city}, ${cities[random1000].state}`,
+      title: `${sample(descriptors)} ${sample(places)}`,
+      image: "https://source.unsplash.com/collection/483251",
+      description:
+        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nostrum nulla distinctio ut quod rerum, adipisci ex quos officia exercitationem vitae dolore accusamus at recusandae nam, facere inventore fuga rem necessitatibus! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nostrum nulla distinctio ut quod rerum, adipisci ex quos officia exercitationem vitae dolore accusamus at recusandae nam, facere inventore fuga rem necessitatibus!",
+      price,
+    });
+    await camp.save();
+  }
 };
 
 seedDB().then(() => {
-    mongoose.connection.close();
+  mongoose.connection.close();
 });
