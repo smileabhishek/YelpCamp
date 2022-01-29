8327896f908e45411893a0a9cf1431c0bd628a83
Abhishek Anand <abhishekanand1138@gmail.com>
2022-01-30 04:40:42 +0530

    "added image upload to cloudinary feature using cloudinary and multer packages"

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -3,12 +3,19 @@ const router = express.Router();
 const campgrounds = require("../controllers/campgrounds");
 const catchAsync = require("../utils/catchAsync");
 const { isLoggedIn, isAuthor, validateCampground } = require("../middleware");
+const multer = require("multer");
+// the multer package helps in saving files to a local directory or on any cloud service provider
+const { storage } = require("../cloudinary");
+// the storage object contains data of our cloudinary account where the images will be stored
+const upload = multer({ storage });
+// sets the destination of the file where it will be stored
 
 router
   .route("/")
   .get(catchAsync(campgrounds.renderIndex))
   .post(
     isLoggedIn,
+    upload.array("image"),
     validateCampground,
     catchAsync(campgrounds.createCampground)
   );
