4c8d3519652ced8aa4771277950cfd3b837fae3a
Abhishek Anand <abhishekanand1138@gmail.com>
2022-01-30 04:54:58 +0530

    "Read part of cloudinary upload CRUD complete"

--- seeds/index.js
+++ seeds/index.js
@@ -22,10 +22,24 @@ seedDB = async () => {
       author: "61e737a4091907bf96d15595",
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
-      image: "https://source.unsplash.com/collection/483251",
+      // image: "https://source.unsplash.com/collection/483251",
       description:
         "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nostrum nulla distinctio ut quod rerum, adipisci ex quos officia exercitationem vitae dolore accusamus at recusandae nam, facere inventore fuga rem necessitatibus! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nostrum nulla distinctio ut quod rerum, adipisci ex quos officia exercitationem vitae dolore accusamus at recusandae nam, facere inventore fuga rem necessitatibus!",
       price,
+      images: [
+        {
+          url: "https://res.cloudinary.com/dfyaqdmh8/image/upload/v1643496382/YelpCamp/vrgxn3zeq0epvqbgpelj.jpg",
+          filename: "YelpCamp/vrgxn3zeq0epvqbgpelj",
+        },
+        {
+          url: "https://res.cloudinary.com/dfyaqdmh8/image/upload/v1643496386/YelpCamp/hc146wlbgmz8b6p8tpwl.jpg",
+          filename: "YelpCamp/hc146wlbgmz8b6p8tpwl",
+        },
+        {
+          url: "https://res.cloudinary.com/dfyaqdmh8/image/upload/v1643496389/YelpCamp/aqmai2tk3uly99k8jomd.jpg",
+          filename: "YelpCamp/aqmai2tk3uly99k8jomd",
+        },
+      ],
     });
     await camp.save();
   }
