88f22b5de7cd4ccd6606916d349d4636c3b5978d
Abhishek Anand <abhishekanand1138@gmail.com>
2022-01-29 13:39:01 +0530

    "Added starability rating to reviews"

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 // serving static files directory
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "/public")));
 
 // sessions configurations
 const sessionConfig = {
