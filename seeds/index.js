7c3912120345c2d15c91ad3cf9ec3749e3382560
Abhishek Anand <abhishekanand1138@gmail.com>
2022-02-19 13:45:48 +0530

    "Added mapbox clustermaps"

--- seeds/index.js
+++ seeds/index.js
@@ -15,7 +15,7 @@ db.once("open", () => {
 const sample = (array) => array[Math.floor(Math.random() * array.length)];
 seedDB = async () => {
   await Campground.deleteMany({});
-  for (let i = 0; i < 50; i++) {
+  for (let i = 0; i < 300; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
     const price = Math.floor(Math.random() * 30) + 10;
     const camp = new Campground({
@@ -29,7 +29,10 @@ seedDB = async () => {
       price,
       geometry: {
         type: "Point",
-        coordinates: [-113.1331, 47.0202],
+        coordinates: [
+          cities[random1000].longitude,
+          cities[random1000].latitude,
+        ],
       },
       images: [
         {
